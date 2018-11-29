d6e2bed66bc07675ddb1d30e241ed497335f056a
AlexisLopez350 <39870674+AlexisLopez350@users.noreply.github.com>
2018-11-29 02:04:11 -0600

    Integrado

---
+++ blankspaces/ViewModels/MaterialViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using blankspaces.Models;
+
+namespace blankspaces.ViewModels
+{
+    public class MaterialViewModel
+    {
+        public AUTOR Autor1 { get; set; }
+        public CATERGORIA Categoria1 { get; set; }
+        public DOCUMENTOLOCALIDAD DocumentoLocalidad1 { get; set; }
+        public MATERIALBIBLIOGRAFICO MaterialBibliografico1 { get; set; }
+        public TIPODOCUMENTO TipoDocumento1 { get; set; }
+        public string IDCATEGORIA { get; set; }
+        public string IDSUBCATEGORIA { get; set; }
+
+    }
+}
